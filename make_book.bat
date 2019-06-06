7425f8461e4424fd71a5952d2c30aefed4741627
larisatomeci <larisatomeci@users.noreply.github.com>
2019-06-06 11:00:12 -0400

    Files adding the figures into the book

--- make_book.bat
+++ make_book.bat
@@ -1,7 +1,7 @@
 @echo off
 cd "C:\Users\lrt9\Documents\MCBook2019"
 rem close the book.pdf file
-
+rem https://stackoverflow.com/questions/5085491/closing-an-instance-of-acrobat-reader-from-command-line 
 pdflatex book.tex
 bibtex book
 makeindex book
