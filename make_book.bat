4899b77edbe4e0fe4c1b88ba615e5724872b1203
larisatomeci <larisatomeci@users.noreply.github.com>
2019-06-07 09:37:34 -0400

    Files with graphs for chapters

--- make_book.bat
+++ make_book.bat
@@ -4,7 +4,7 @@ rem close the book.pdf file
 rem https://stackoverflow.com/questions/5085491/closing-an-instance-of-acrobat-reader-from-command-line 
 pdflatex book.tex
 bibtex book
-makeindex book
+rem makeindex book
 pdflatex book.tex
 pdflatex book.tex
 rem open the file in default application (Acrobat)
