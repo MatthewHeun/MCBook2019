733ba28ed5001643ba762e0fdf88f4b5939eb711
Matthew Kuperus Heun <matthew.heun@me.com>
2019-06-15 12:29:48 -0400

    Add a clean script for UNIX.

---
+++ clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Script to clean up auxiliary files on UNIX-based systems like macOS.
+# Run this script first if you need a clean compile of the book.
+
+rm book-concordance.tex
+rm book.aux
+rm book.bbl
+rm book.blg
+rm book.idx
+rm book.ilg
+rm book.ind
+rm book.log
+rm book.out
+rm book.pdf
+rm book.synctex.gz
+rm book.tex
+rm book.toc
